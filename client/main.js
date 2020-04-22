uber/cadence-web

Feature/workflows refactoring (#112)
* refactoring workflow list screen

* fixing spinner

* removing settings cog and workflows from breadcrumb

* eslint and small refactor for criteria

* fixing tests

* 3.14.0-beta.0

client/main.js
@@ -13,17 +13,19 @@ import copyButton from './components/copy.vue';
 import snapscroll from './directives/snapscroll';
 
 import App from './App.vue';
-import Root from './routes/index.vue';
-import Help from './routes/help/index.vue';
+import Domain from './routes/domain/index.vue';
 import DomainList from './routes/domain-list.vue';
-import WorkflowList from './routes/domain/workflow-list.vue';
-import DomainConfig from './routes/domain/domain-config.vue';
-import WorkflowTabs from './routes/workflow/index.vue';
-import WorkflowSummary from './routes/workflow/summary.vue';
+import DomainSettings from './routes/domain/domain-settings.vue';
+import Help from './routes/help/index.vue';
 import History from './routes/workflow/history.vue';
-import StackTrace from './routes/workflow/stack-trace.vue';
 import Query from './routes/workflow/query.vue';
+import Root from './routes/index.vue';
+import StackTrace from './routes/workflow/stack-trace.vue';
 import TaskList from './routes/domain/task-list.vue';
+import WorkflowList from './routes/domain/workflow-list.vue';
+import WorkflowSummary from './routes/workflow/summary.vue';
+import WorkflowTabs from './routes/workflow/index.vue';
+
 import { http, injectMomentDurationFormat, jsonTryParse } from '~helpers';
 
 const routeOpts = {
@@ -51,22 +53,28 @@ const routeOpts = {
       ],
     },
     {
-      name: 'domains-redirect',
-      path: '/domain/*',
-      redirect: '/domains/*',
-    },
-    {
-      name: 'workflow-list',
-      path: '/domains/:domain/workflows',
-      component: WorkflowList,
-    },
-    {
-      name: 'domain-config',
-      path: '/domains/:domain/config',
-      component: DomainConfig,
+      name: 'domain',
+      path: '/domains/:domain',
+      component: Domain,
       props: ({ params }) => ({
         domain: params.domain,
       }),
+      children: [
+        {
+          name: 'workflow-list',
+          path: '/domains/:domain/workflows',
+          components: {
+            'workflow-list': WorkflowList,
+          },
+        },
+        {
+          name: 'domain-settings',
+          path: '/domains/:domain/settings',
+          components: {
+            'domain-settings': DomainSettings,
+          },
+        },
+      ],
     },
     {
       name: 'workflow',
@@ -128,6 +136,19 @@ const routeOpts = {
       name: 'task-list',
       path: '/domains/:domain/task-lists/:taskList',
       component: TaskList,
+    },
+
+    // redirects
+
+    {
+      name: 'domains-redirect',
+      path: '/domain/*',
+      redirect: '/domains/*',
+    },
+    {
+      name: 'domain-config-redirect',
+      path: '/domains/:domain/config',
+      redirect: '/domains/:domain/settings',
     },
     {
       path: '/domains/:domain/history',

client/routes/domain/workflow-list.vue
@@ -64,6 +64,8 @@
         filterMode === 'advanced' ? 'basic' : 'advanced'
       }}</a>
     </header>
+    <span class="error" v-if="error">{{ error }}</span>
+    <span class="no-results" v-if="showNoResults">No Results</span>
     <section
       class="results"
       v-infinite-scroll="nextPage"
@@ -100,8 +102,6 @@
         </tbody>
       </table>
     </section>
-    <span class="error" v-if="error">{{ error }}</span>
-    <span class="no-results" v-if="showNoResults">No Results</span>
   </section>
 </template>
 
@@ -112,6 +112,7 @@ import pagedGrid from '~components/paged-grid';
 import { DateRangePicker } from '~components';
 
 export default pagedGrid({
+  props: ['domain'],
   data() {
     return {
       loading: true,
@@ -133,13 +134,13 @@ export default pagedGrid({
     };
   },
   created() {
-    this.$http(`/api/domains/${this.$route.params.domain}`).then(r => {
+    this.$http(`/api/domains/${this.domain}`).then(r => {
       this.maxRetentionDays =
         Number(r.configuration.workflowExecutionRetentionPeriodInDays) || 30;
 
       if (!this.isRouteRangeValid(this.minStartDate)) {
         const prevRange = localStorage.getItem(
-          `${this.$route.params.domain}:workflows-time-range`
+          `${this.domain}:workflows-time-range`
         );
 
         if (prevRange && this.isRangeValid(prevRange, this.minStartDate)) {
@@ -164,79 +165,107 @@ export default pagedGrid({
     'date-range-picker': DateRangePicker,
   },
   computed: {
+    fetchUrl() {
+      const { domain, queryString, state } = this;
+
+      if (queryString) {
+        return `/api/domains/${domain}/workflows/list`;
+      }
+
+      return `/api/domains/${domain}/workflows/${state}`;
+    },
+    endTime() {
+      const { endTime, range } = this.$route.query;
+
+      return this.getEndTimeIsoString(range, endTime);
+    },
     filterBy() {
       return this.status.value === 'OPEN' ? 'StartTime' : 'CloseTime';
     },
-    status() {
-      if (!this.$route.query || !this.$route.query.status) {
-        return this.statuses[0];
-      }
+    startTime() {
+      const { range, startTime } = this.$route.query;
 
-      return this.statuses.find(s => s.value === this.$route.query.status);
+      return this.getStartTimeIsoString(range, startTime);
+    },
+    state() {
+      const { statusName } = this;
+
+      return !statusName || statusName === 'OPEN' ? 'open' : 'closed';
+    },
+    status() {
+      return !this.$route.query || !this.$route.query.status
+        ? this.statuses[0]
+        : this.statuses.find(s => s.value === this.$route.query.status);
+    },
+    statusName() {
+      return this.status.value;
     },
     range() {
-      const q = this.$route.query || {};
-
-      return q.startTime && q.endTime
-        ? {
-            startTime: moment(q.startTime),
-            endTime: moment(q.endTime),
-          }
-        : q.range;
-    },
-    criteria() {
-      const { domain } = this.$route.params;
-      const q = this.$route.query;
-      const startTime = this.getStartTimeIsoString(q.range, q.startTime);
-      const endTime = this.getEndTimeIsoString(q.range, q.endTime);
-
-      this.nextPageToken = undefined;
-
-      return {
-        domain,
-        startTime,
-        endTime,
-        status: q.status,
-        workflowId: q.workflowId,
-        workflowName: q.workflowName,
-        queryString: q.queryString,
-      };
-    },
-    queryOnChange() {
-      const q = { ...this.criteria };
-      const { domain } = q;
-      const state = !q.status || q.status === 'OPEN' ? 'open' : 'closed';
-
-      if (!q.startTime || !q.endTime || !q.status) {
-        return;
-      }
+      const query = this.$route.query || {};
 
       if (!this.isRouteRangeValid(this.minStartDate)) {
         const updatedQuery = this.setRange(
           `last-${Math.min(30, this.maxRetentionDays)}-days`
         );
 
-        q.startTime = this.getStartTimeIsoString(
+        query.startTime = this.getStartTimeIsoString(
           updatedQuery.range,
-          q.startTime
+          query.startTime
         );
-        q.endTime = this.getEndTimeIsoString(updatedQuery.range, q.endTime);
+        query.endTime = this.getEndTimeIsoString(
+          updatedQuery.range,
+          query.endTime
+        );
       }
 
-      if (['OPEN', 'CLOSED'].includes(q.status)) {
-        delete q.status;
+      return query.startTime && query.endTime
+        ? {
+            startTime: moment(query.startTime),
+            endTime: moment(query.endTime),
+          }
+        : query.range;
+    },
+    criteria() {
+      const {
+        endTime,
+        queryString,
+        startTime,
+        statusName: status,
+        workflowId,
+        workflowName,
+      } = this;
+
+      this.nextPageToken = undefined;
+
+      if (!startTime || !endTime) {
+        return null;
       }
 
-      delete q.domain;
-      q.nextPageToken = this.nextPageToken;
+      const includeStatus = !['OPEN', 'CLOSED'].includes(status);
 
-      if (q.queryString) {
-        this.fetch(`/api/domains/${domain}/workflows/list`, q);
+      const criteria = {
+        startTime,
+        endTime,
+        ...(queryString && { queryString }),
+        ...(includeStatus && { status }),
+        ...(workflowId && { workflowId }),
+        ...(workflowName && { workflowName }),
+      };
 
+      return criteria;
+    },
+    queryOnChange() {
+      if (!this.criteria) {
         return;
       }
 
-      this.fetch(`/api/domains/${domain}/workflows/${state}`, q);
+      const { fetchUrl, nextPageToken } = this;
+      const query = { ...this.criteria, nextPageToken };
+
+      this.fetch(fetchUrl, query);
+    },
+    queryString() {
+      return this.$route.query.queryString;
     },
     minStartDate() {
       const {
@@ -251,6 +280,12 @@ export default pagedGrid({
       return moment(this.now)
         .subtract(maxRetentionDays, 'days')
         .startOf('days');
+    },
+    workflowId() {
+      return this.$route.query.workflowId;
+    },
+    workflowName() {
+      return this.$route.query.workflowName;
     },
   },
   methods: {
@@ -367,8 +402,7 @@ export default pagedGrid({
       return false;
     },
     isRouteRangeValid(minStartDate) {
-      const q = this.$route.query || {};
-      const { endTime, range, startTime } = q;
+      const { endTime, range, startTime } = this.$route.query || {};
 
       if (range) {
         return this.isRangeValid(range, minStartDate);
@@ -388,10 +422,7 @@ export default pagedGrid({
           query.range = range;
           delete query.startTime;
           delete query.endTime;
-          localStorage.setItem(
-            `${this.$route.params.domain}:workflows-time-range`,
-            range
-          );
+          localStorage.setItem(`${this.domain}:workflows-time-range`, range);
         } else {
           query.startTime = range.startTime.toISOString();
           query.endTime = range.endTime.toISOString();
@@ -423,10 +454,27 @@ export default pagedGrid({
 @require "../../styles/definitions.styl"
 
 section.workflow-list
+  display: flex;
+  flex: auto;
+  flex-direction: column;
+
   .filters
-    flex-wrap wrap
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+
     > .field
       flex 1 1 auto
+      margin-right: 5px;
+
+    .date-range-picker {
+      margin-right: 5px;
+    }
+
+    .dropdown {
+      margin-right: 5px;
+    }
+
     .status {
       width: 160px;
     }
@@ -437,6 +485,10 @@ section.workflow-list
       action-button()
 
   paged-grid()
+
+  section.results {
+    flex: auto;
+  }
 
   &.loading section.results table
     opacity 0.7

client/test/index.js
@@ -138,7 +138,7 @@ require('./domain-list.test');
 require('./help.test');
 require('./workflow-list.test');
 require('./workflow.test');
-require('./domain-config.test');
+require('./domain-settings.test');
 require('./task-list.test');
 
 mocha.run();

temporalio/web

client/main.js
@@ -13,17 +13,19 @@ import copyButton from './components/copy.vue';
 import snapscroll from './directives/snapscroll';
 
 import App from './App.vue';
-import Root from './routes/index.vue';
-import Help from './routes/help/index.vue';
+import Namespace from './routes/namespace/index.vue';
 import NamespaceList from './routes/namespace-list.vue';
-import WorkflowList from './routes/namespace/workflow-list.vue';
-import NamespaceConfig from './routes/namespace/namespace-config.vue';
-import WorkflowTabs from './routes/workflow/index.vue';
-import WorkflowSummary from './routes/workflow/summary.vue';
+import NamespaceSettings from './routes/namespace/namespace-settings.vue';
+import Help from './routes/help/index.vue';
 import History from './routes/workflow/history.vue';
-import StackTrace from './routes/workflow/stack-trace.vue';
 import Query from './routes/workflow/query.vue';
+import Root from './routes/index.vue';
+import StackTrace from './routes/workflow/stack-trace.vue';
 import TaskList from './routes/namespace/task-list.vue';
+import WorkflowList from './routes/namespace/workflow-list.vue';
+import WorkflowSummary from './routes/workflow/summary.vue';
+import WorkflowTabs from './routes/workflow/index.vue';
+
 import { http, injectMomentDurationFormat, jsonTryParse } from '~helpers';
 
 const routeOpts = {
@@ -51,22 +53,28 @@ const routeOpts = {
       ],
     },
     {
-      name: 'namespaces-redirect',
-      path: '/namespace/*',
-      redirect: '/namespaces/*',
-    },
-    {
-      name: 'workflow-list',
-      path: '/namespaces/:namespace/workflows',
-      component: WorkflowList,
-    },
-    {
-      name: 'namespace-config',
-      path: '/namespaces/:namespace/config',
-      component: NamespaceConfig,
+      name: 'namespace',
+      path: '/namespaces/:namespace',
+      component: Namespace,
       props: ({ params }) => ({
         namespace: params.namespace,
       }),
+      children: [
+        {
+          name: 'workflow-list',
+          path: '/namespaces/:namespace/workflows',
+          components: {
+            'workflow-list': WorkflowList,
+          },
+        },
+        {
+          name: 'namespace-settings',
+          path: '/namespaces/:namespace/settings',
+          components: {
+            'namespace-settings': NamespaceSettings,
+          },
+        },
+      ],
     },
     {
       name: 'workflow',
@@ -128,6 +136,19 @@ const routeOpts = {
       name: 'task-list',
       path: '/namespaces/:namespace/task-lists/:taskList',
       component: TaskList,
+    },
+
+    // redirects
+
+    {
+      name: 'namespaces-redirect',
+      path: '/namespace/*',
+      redirect: '/namespaces/*',
+    },
+    {
+      name: 'namespace-config-redirect',
+      path: '/namespaces/:namespace/config',
+      redirect: '/namespaces/:namespace/settings',
     },
     {
       path: '/namespaces/:namespace/history',

client/routes/namespace/workflow-list.vue
@@ -64,6 +64,8 @@
         filterMode === 'advanced' ? 'basic' : 'advanced'
       }}</a>
     </header>
+    <span class="error" v-if="error">{{ error }}</span>
+    <span class="no-results" v-if="showNoResults">No Results</span>
     <section
       class="results"
       v-infinite-scroll="nextPage"
@@ -100,8 +102,6 @@
         </tbody>
       </table>
     </section>
-    <span class="error" v-if="error">{{ error }}</span>
-    <span class="no-results" v-if="showNoResults">No Results</span>
   </section>
 </template>
 
@@ -113,6 +113,7 @@ import { DateRangePicker } from '~components';
 import { timestampToDate } from '~helpers';
 
 export default pagedGrid({
+  props: ['namespace'],
   data() {
     return {
       loading: true,
@@ -134,13 +135,13 @@ export default pagedGrid({
     };
   },
   created() {
-    this.$http(`/api/namespaces/${this.$route.params.namespace}`).then(r => {
+    this.$http(`/api/namespaces/${this.namespace}`).then(r => {
       this.maxRetentionDays =
         Number(r.configuration.workflowExecutionRetentionPeriodInDays) || 30;
 
       if (!this.isRouteRangeValid(this.minStartDate)) {
         const prevRange = localStorage.getItem(
-          `${this.$route.params.namespace}:workflows-time-range`
+          `${this.namespace}:workflows-time-range`
         );
 
         if (prevRange && this.isRangeValid(prevRange, this.minStartDate)) {
@@ -165,79 +166,107 @@ export default pagedGrid({
     'date-range-picker': DateRangePicker,
   },
   computed: {
+    fetchUrl() {
+      const { namespace, queryString, state } = this;
+
+      if (queryString) {
+        return `/api/namespaces/${namespace}/workflows/list`;
+      }
+
+      return `/api/namespaces/${namespace}/workflows/${state}`;
+    },
+    endTime() {
+      const { endTime, range } = this.$route.query;
+
+      return this.getEndTimeIsoString(range, endTime);
+    },
     filterBy() {
       return this.status.value === 'OPEN' ? 'StartTime' : 'CloseTime';
     },
-    status() {
-      if (!this.$route.query || !this.$route.query.status) {
-        return this.statuses[0];
-      }
+    startTime() {
+      const { range, startTime } = this.$route.query;
 
-      return this.statuses.find(s => s.value === this.$route.query.status);
+      return this.getStartTimeIsoString(range, startTime);
+    },
+    state() {
+      const { statusName } = this;
+
+      return !statusName || statusName === 'OPEN' ? 'open' : 'closed';
+    },
+    status() {
+      return !this.$route.query || !this.$route.query.status
+        ? this.statuses[0]
+        : this.statuses.find(s => s.value === this.$route.query.status);
+    },
+    statusName() {
+      return this.status.value;
     },
     range() {
-      const q = this.$route.query || {};
-
-      return q.startTime && q.endTime
-        ? {
-            startTime: moment(q.startTime),
-            endTime: moment(q.endTime),
-          }
-        : q.range;
-    },
-    criteria() {
-      const { namespace } = this.$route.params;
-      const q = this.$route.query;
-      const startTime = this.getStartTimeIsoString(q.range, q.startTime);
-      const endTime = this.getEndTimeIsoString(q.range, q.endTime);
-
-      this.nextPageToken = undefined;
-
-      return {
-        namespace,
-        startTime,
-        endTime,
-        status: q.status,
-        workflowId: q.workflowId,
-        workflowName: q.workflowName,
-        queryString: q.queryString,
-      };
-    },
-    queryOnChange() {
-      const q = { ...this.criteria };
-      const { namespace } = q;
-      const state = !q.status || q.status === 'OPEN' ? 'open' : 'closed';
-
-      if (!q.startTime || !q.endTime || !q.status) {
-        return;
-      }
+      const query = this.$route.query || {};
 
       if (!this.isRouteRangeValid(this.minStartDate)) {
         const updatedQuery = this.setRange(
           `last-${Math.min(30, this.maxRetentionDays)}-days`
         );
 
-        q.startTime = this.getStartTimeIsoString(
+        query.startTime = this.getStartTimeIsoString(
           updatedQuery.range,
-          q.startTime
+          query.startTime
         );
-        q.endTime = this.getEndTimeIsoString(updatedQuery.range, q.endTime);
+        query.endTime = this.getEndTimeIsoString(
+          updatedQuery.range,
+          query.endTime
+        );
       }
 
-      if (['OPEN', 'CLOSED'].includes(q.status)) {
-        delete q.status;
+      return query.startTime && query.endTime
+        ? {
+            startTime: moment(query.startTime),
+            endTime: moment(query.endTime),
+          }
+        : query.range;
+    },
+    criteria() {
+      const {
+        endTime,
+        queryString,
+        startTime,
+        statusName: status,
+        workflowId,
+        workflowName,
+      } = this;
+
+      this.nextPageToken = undefined;
+
+      if (!startTime || !endTime) {
+        return null;
       }
 
-      delete q.namespace;
-      q.nextPageToken = this.nextPageToken;
+      const includeStatus = !['OPEN', 'CLOSED'].includes(status);
 
-      if (q.queryString) {
-        this.fetch(`/api/namespaces/${namespace}/workflows/list`, q);
+      const criteria = {
+        startTime,
+        endTime,
+        ...(queryString && { queryString }),
+        ...(includeStatus && { status }),
+        ...(workflowId && { workflowId }),
+        ...(workflowName && { workflowName }),
+      };
 
+      return criteria;
+    },
+    queryOnChange() {
+      if (!this.criteria) {
         return;
       }
 
-      this.fetch(`/api/namespaces/${namespace}/workflows/${state}`, q);
+      const { fetchUrl, nextPageToken } = this;
+      const query = { ...this.criteria, nextPageToken };
+
+      this.fetch(fetchUrl, query);
+    },
+    queryString() {
+      return this.$route.query.queryString;
     },
     minStartDate() {
       const {
@@ -252,6 +281,12 @@ export default pagedGrid({
       return moment(this.now)
         .subtract(maxRetentionDays, 'days')
         .startOf('days');
+    },
+    workflowId() {
+      return this.$route.query.workflowId;
+    },
+    workflowName() {
+      return this.$route.query.workflowName;
     },
   },
   methods: {
@@ -377,8 +412,7 @@ export default pagedGrid({
       return false;
     },
     isRouteRangeValid(minStartDate) {
-      const q = this.$route.query || {};
-      const { endTime, range, startTime } = q;
+      const { endTime, range, startTime } = this.$route.query || {};
 
       if (range) {
         return this.isRangeValid(range, minStartDate);
@@ -398,10 +432,7 @@ export default pagedGrid({
           query.range = range;
           delete query.startTime;
           delete query.endTime;
-          localStorage.setItem(
-            `${this.$route.params.namespace}:workflows-time-range`,
-            range
-          );
+          localStorage.setItem(`${this.namespace}:workflows-time-range`, range);
         } else {
           query.startTime = range.startTime.toISOString();
           query.endTime = range.endTime.toISOString();
@@ -433,10 +464,27 @@ export default pagedGrid({
 @require "../../styles/definitions.styl"
 
 section.workflow-list
+  display: flex;
+  flex: auto;
+  flex-direction: column;
+
   .filters
-    flex-wrap wrap
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+
     > .field
       flex 1 1 auto
+      margin-right: 5px;
+
+    .date-range-picker {
+      margin-right: 5px;
+    }
+
+    .dropdown {
+      margin-right: 5px;
+    }
+
     .status {
       width: 160px;
     }
@@ -447,6 +495,10 @@ section.workflow-list
       action-button()
 
   paged-grid()
+
+  section.results {
+    flex: auto;
+  }
 
   &.loading section.results table
     opacity 0.7

client/test/index.js
@@ -138,7 +138,7 @@ require('./namespace-list.test');
 require('./help.test');
 require('./workflow-list.test');
 require('./workflow.test');
-require('./namespace-config.test');
+require('./namespace-settings.test');
 require('./task-list.test');
 
 mocha.run();