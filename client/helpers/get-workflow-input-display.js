c99be0983aa38847abf2fc12240dba6f55814414
feedmeapples <aksenov.ro@outlook.com>
2020-05-14 18:12:30 -0700

    Clean out workflow input helper

--- client/helpers/get-workflow-input-display.js
+++ client/helpers/get-workflow-input-display.js
@@ -1,19 +1,9 @@
 export default input => {
-  if (!input) {
+  if (!input || !input.payloads) {
     return input;
   }
 
-  let parsed = {};
-
-  if (input.jsonStringFull) {
-    parsed = JSON.tryParse(input.jsonStringFull);
-  } else if (input.payloads !== undefined) {
-    parsed = input;
-  } else {
-    return input;
-  }
-
-  const args = parsed.payloads.map((p, i) => ({ [`arg[${i}]`]: p.data }));
+  const args = input.payloads.map((p, i) => ({ [`arg[${i}]`]: p.data }));
 
   return args;
 };
