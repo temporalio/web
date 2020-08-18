f1578bf39b7d5eadec38941d8d85833f802eefef
Ruslan <11838981+feedmeapples@users.noreply.github.com>
2020-08-18 14:07:11 -0700

    Create namespace e2e testing (#106)

---
+++ cypress/integration/workflow/namespace.spec.js
@@ -0,0 +1,49 @@
+/// <reference types="cypress" />
+
+context('Namespace', () => {
+  beforeEach(() => {
+    cy.visit('/namespaces');
+  });
+
+  it('render namespace details', () => {
+    cy.get('[data-cy=recent-namespaces]')
+      .contains('namespace-web-e2e')
+      .trigger('mouseover');
+
+    cy.get('[data-cy=namespace-description]')
+      .should('contain.text', 'namespace-web-e2e') // Namespace name
+      .should('contain.text', 'description-for-web-e2e') // Description
+      .should('contain.text', 'user0@test.com') // Owner
+      .should('contain.text', '5 days') // Retention Period
+      .should('contain.text', 'Enabled') // History & Visibility statuses
+      .should('not.contain.text', 'Disabled')
+      .should('contain.text', 'active (active)'); // Clusters
+  });
+
+  it('navigate to recent namespace', () => {
+    cy.get('[data-cy=recent-namespaces]')
+      .contains('namespace-web-e2e')
+      .click();
+
+    cy.url().should('include', '/namespaces/namespace-web-e2e/workflows');
+  });
+
+  it('render namespace settings', () => {
+    cy.get('[data-cy=recent-namespaces]')
+      .contains('namespace-web-e2e')
+      .click();
+
+    cy.get('[data-cy=namespace-settings-link]').click();
+
+    cy.url().should('include', '/namespaces/namespace-web-e2e/settings');
+
+    cy.get('[data-cy=namespace-settings]')
+      .should('contain.text', 'namespace-web-e2e') // Namespace name
+      .should('contain.text', 'description-for-web-e2e') // Description
+      .should('contain.text', 'user0@test.com') // Owner
+      .should('contain.text', '5 days') // Retention Period
+      .should('contain.text', 'Enabled') // History & Visibility statuses
+      .should('not.contain.text', 'Disabled')
+      .should('contain.text', 'active (active)'); // Clusters
+  });
+});
