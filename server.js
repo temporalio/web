eb2840c0dadaa04de6304aeaeb66465ec55dde67
John M. Arnesen <john@macbook-pro-16.arnesenfamily.org>
2021-08-30 14:32:39 -0400

    adding support for running temporal web using https to the container

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
-var app = require('./server/index'),
-    port = Number(process.env.TEMPORAL_WEB_PORT) || 8088,
-    production = process.env.NODE_ENV === 'production'
+const app = require('./server/index'),
+  port = Number(process.env.TEMPORAL_WEB_PORT) || 8088,
+  production = process.env.NODE_ENV === 'production',
+  sslEnabled = process.env.TEMPORAL_WEB_USE_HTTPS || false;
 
-app.init().listen(port)
+if (sslEnabled) {
+  const https = require('https');
+  const fs = require('fs');
+  const options = {
+    key: fs.readFileSync(process.env.TEMPORAL_WEB_TLS_KEY_PATH),
+    cert: fs.readFileSync(process.env.TEMPORAL_WEB_TLS_CERT_PATH),
+  };
 
-console.log('temporal-web up and listening on port ' + port)
+  https.createServer(options, app.init().callback()).listen(port);
+} else {
+  app.init().listen(port);
+}
+
+console.log('temporal-web up and listening on port ' + port);
+
 if (!production) {
-  console.log('webpack is compiling...')
+  console.log('webpack is compiling...');
 }
