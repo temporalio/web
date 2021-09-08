b5f459a1b61a3272a31341cd42a6dbb9452a4bb3
arnesenfamily <arnesenfamily@users.noreply.github.com>
2021-09-08 16:14:50 -0400

    Add support for built-in HTTPS (#379)
    * adding support for running temporal web using HTTPS based on ENV vars

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
-var app = require('./server/index'),
-    port = Number(process.env.TEMPORAL_WEB_PORT) || 8088,
-    production = process.env.NODE_ENV === 'production'
+const app = require('./server/index'),
+  port = Number(process.env.TEMPORAL_WEB_PORT) || 8088,
+  production = process.env.NODE_ENV === 'production',
+  sslEnabled = process.env.TEMPORAL_WEB_TLS_CERT_PATH != null;
 
-app.init().listen(port)
+if (sslEnabled) {
+  const https = require('https');
+  const fs = require('fs');
 
-console.log('temporal-web up and listening on port ' + port)
+  console.log('temporal-web ssl is enabled');
+  https.createServer({
+      key: fs.readFileSync(process.env.TEMPORAL_WEB_TLS_KEY_PATH),
+      cert: fs.readFileSync(process.env.TEMPORAL_WEB_TLS_CERT_PATH),
+    }, 
+    app.init().callback()
+  ).listen(port);
+} else {
+  console.log('temporal-web ssl is not enabled');
+  app.init().listen(port);
+}
+
+console.log('temporal-web up and listening on port ' + port);
+
 if (!production) {
-  console.log('webpack is compiling...')
+  console.log('webpack is compiling...');
 }
